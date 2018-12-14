2af9fb654da2fe31e0342305ae3d24001a9b5d03
Seyed Naser RAZAVI <razavinaser@gmail.com>
2018-12-15 00:13:59 +0330

    Add files via upload

---
+++ 9-DimensionallityReduction/plot_utils.py
@@ -0,0 +1,140 @@
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+
+
+def plot_image_components(x, coefficients=None, mean=0, components=None,
+                          imshape=(8, 8), n_components=6, fontsize=12):
+    if coefficients is None:
+        coefficients = x
+        
+    if components is None:
+        components = np.eye(len(coefficients), len(x))
+        
+    mean = np.zeros_like(x) + mean
+        
+
+    fig = plt.figure(figsize=(1.2 * (5 + n_components), 1.2 * 2))
+    g = plt.GridSpec(2, 5 + n_components, hspace=0.3)
+
+    def show(i, j, x, title=None):
+        ax = fig.add_subplot(g[i, j], xticks=[], yticks=[])
+        ax.imshow(x.reshape(imshape), interpolation='nearest')
+        if title:
+            ax.set_title(title, fontsize=fontsize)
+
+    show(slice(2), slice(2), x, "True")
+
+    approx = mean.copy()
+    show(0, 2, np.zeros_like(x) + mean, r'$\mu$')
+    show(1, 2, approx, r'$1 \cdot \mu$')
+
+    for i in range(0, n_components):
+        approx = approx + coefficients[i] * components[i]
+        show(0, i + 3, components[i], r'$c_{0}$'.format(i + 1))
+        show(1, i + 3, approx,
+             r"${0:.2f} \cdot c_{1}$".format(coefficients[i], i + 1))
+        plt.gca().text(0, 1.05, '$+$', ha='right', va='bottom',
+                       transform=plt.gca().transAxes, fontsize=fontsize)
+
+    show(slice(2), slice(-2, None), approx, "Approx")
+
+
+def plot_pca_interactive(data, n_components=6):
+    from sklearn.decomposition import PCA
+    #from IPython.html.widgets import interact
+    from ipywidgets import interact
+
+    pca = PCA(n_components=n_components)
+    Xproj = pca.fit_transform(data)
+
+    def show_decomp(i=0):
+        plot_image_components(data[i], Xproj[i],
+                              pca.mean_, pca.components_)
+    
+    interact(show_decomp, i=(0, data.shape[0] - 1));
+
+
+def plot_pca_illustration():
+    rnd = np.random.RandomState(5)
+    X_ = rnd.normal(size=(300, 2))
+    X_blob = np.dot(X_, rnd.normal(size=(2, 2))) + rnd.normal(size=2)
+
+    pca = PCA()
+    pca.fit(X_blob)
+    X_pca = pca.transform(X_blob)
+
+    S = X_pca.std(axis=0)
+
+    fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+    axes = axes.ravel()
+
+    axes[0].set_title("Original data")
+    axes[0].scatter(X_blob[:, 0], X_blob[:, 1], c=X_pca[:, 0], linewidths=0,
+                    s=60, cmap='viridis')
+    axes[0].set_xlabel("feature 1")
+    axes[0].set_ylabel("feature 2")
+    axes[0].arrow(pca.mean_[0], pca.mean_[1], S[0] * pca.components_[0, 0],
+                  S[0] * pca.components_[0, 1], width=.1, head_width=.3,
+                  color='k')
+    axes[0].arrow(pca.mean_[0], pca.mean_[1], S[1] * pca.components_[1, 0],
+                  S[1] * pca.components_[1, 1], width=.1, head_width=.3,
+                  color='k')
+    axes[0].text(-1.5, -.5, "Component 2", size=14)
+    axes[0].text(-4, -4, "Component 1", size=14)
+    axes[0].set_aspect('equal')
+
+    axes[1].set_title("Transformed data")
+    axes[1].scatter(X_pca[:, 0], X_pca[:, 1], c=X_pca[:, 0], linewidths=0,
+                    s=60, cmap='viridis')
+    axes[1].set_xlabel("First principal component")
+    axes[1].set_ylabel("Second principal component")
+    axes[1].set_aspect('equal')
+    axes[1].set_ylim(-8, 8)
+
+    pca = PCA(n_components=1)
+    pca.fit(X_blob)
+    X_inverse = pca.inverse_transform(pca.transform(X_blob))
+
+    axes[2].set_title("Transformed data w/ second component dropped")
+    axes[2].scatter(X_pca[:, 0], np.zeros(X_pca.shape[0]), c=X_pca[:, 0],
+                    linewidths=0, s=60, cmap='viridis')
+    axes[2].set_xlabel("First principal component")
+    axes[2].set_aspect('equal')
+    axes[2].set_ylim(-8, 8)
+
+    axes[3].set_title("Back-rotation using only first component")
+    axes[3].scatter(X_inverse[:, 0], X_inverse[:, 1], c=X_pca[:, 0],
+                    linewidths=0, s=60, cmap='viridis')
+    axes[3].set_xlabel("feature 1")
+    axes[3].set_ylabel("feature 2")
+    axes[3].set_aspect('equal')
+    axes[3].set_xlim(-8, 4)
+    axes[3].set_ylim(-8, 4)
+
+
+def plot_pca_whitening():
+    rnd = np.random.RandomState(5)
+    X_ = rnd.normal(size=(300, 2))
+    X_blob = np.dot(X_, rnd.normal(size=(2, 2))) + rnd.normal(size=2)
+
+    pca = PCA(whiten=True)
+    pca.fit(X_blob)
+    X_pca = pca.transform(X_blob)
+
+    fig, axes = plt.subplots(1, 2, figsize=(10, 10))
+    axes = axes.ravel()
+
+    axes[0].set_title("Original data")
+    axes[0].scatter(X_blob[:, 0], X_blob[:, 1], c=X_pca[:, 0], linewidths=0, s=60, cmap='viridis')
+    axes[0].set_xlabel("feature 1")
+    axes[0].set_ylabel("feature 2")
+    axes[0].set_aspect('equal')
+
+    axes[1].set_title("Whitened data")
+    axes[1].scatter(X_pca[:, 0], X_pca[:, 1], c=X_pca[:, 0], linewidths=0, s=60, cmap='viridis')
+    axes[1].set_xlabel("First principal component")
+    axes[1].set_ylabel("Second principal component")
+    axes[1].set_aspect('equal')
+    axes[1].set_xlim(-3, 4)
